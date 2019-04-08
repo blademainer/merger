4269ef5f0a18e4c24c4ebdd36055a1a13bb54947
blademainer <blademainer@gmail.com>
2019-04-09 01:10:37 +0800

    init

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"github.com/blademainer/merger/processor"
 )
 
-
-
 func main() {
-	p := &processor.Processor{}
 	sourceDir := flag.String("s", "", "source dir")
 	targetDir := flag.String("t", "", "target dir")
-	removeSource := flag.Bool("d", true, "remove source dir")
+	removeSource := flag.Bool("r", true, "remove source dir")
+	concurrent := flag.Uint("c", 100, "remove source dir")
+	debug := flag.Bool("d", true, "remove source dir")
 	flag.Parse()
 	fmt.Println("sourceDir: ", *sourceDir)
 	fmt.Println("targetDir: ", *targetDir)
 	fmt.Println("deleteSource: ", *removeSource)
-
+	p, e := processor.InitProcessor(*sourceDir, *targetDir, *removeSource, int(*concurrent), *debug)
+	if e != nil {
+		panic(e)
+	}
+	p.Process()
 }
